e819b8e72bc1774fdf1ddbfa54cb22a9588cf467
John Filippone <admin@austinmusiclove.com>
2024-11-02 01:24:18 -0500

    Adds get performances by performer api

--- wp-content/mu-plugins/performances-api.php
+++ wp-content/mu-plugins/performances-api.php
@@ -77,3 +77,52 @@ add_action('rest_api_init', function () {
     ]);
 });
 
+
+function get_performances_by_performer() {
+    $start_date = $_GET['start_date'];
+    $end_date = $_GET['end_date'];
+    $performing_act_name = stripslashes($_GET['performing_act_name']);
+    $args = array(
+        'post_type' => 'performance',
+        'nopaging' => true,
+        'meta_query' => array(
+            'relation' => 'AND',
+            array(
+                'key' => 'performance_date',
+                'value' => $start_date,
+                'compare' => '>=',
+                'type' => 'DATE'
+            ),
+            array(
+                'key' => 'performance_date',
+                'value' => $end_date,
+                'compare' => '<',
+                'type' => 'DATE'
+            ),
+            array(
+                'key' => 'performing_act_name',
+                'value' => $performing_act_name,
+                'compare' => '='
+            )
+        ),
+    );
+    $query = new WP_Query($args);
+    if ($query->have_posts()) {
+        $query->the_post();
+        return array(
+            'id' => get_the_ID(),
+            'performance_id' => get_field('performance_id'),
+            'venue' => get_field('venue'),
+            'venue_name' => get_field('venue_name'),
+            'performing_act_name' => get_field('performing_act_name'),
+            'performance_date' => get_field('performance_date'),
+        );
+    }
+    return 0;
+}
+add_action('rest_api_init', function () {
+    register_rest_route( 'v1', 'performances/performer', [
+        'methods' => 'GET',
+        'callback' => 'get_performances_by_performer',
+    ]);
+});
