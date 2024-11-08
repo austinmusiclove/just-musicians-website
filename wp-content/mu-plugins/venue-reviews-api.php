92e8cd9389492cc7a979ec01094f0e214df04bf9
John Filippone <admin@austinmusiclove.com>
2024-11-08 04:25:37 -0600

    Debug

--- wp-content/mu-plugins/venue-reviews-api.php
+++ wp-content/mu-plugins/venue-reviews-api.php
@@ -6,6 +6,13 @@ function get_venue_reviews() {
     $args = array(
         'post_type' => 'venue_review',
         'nopaging' => true,
+        'meta_query' => array(
+            array(
+                'key' => 'pay_method',
+                'value' => 'Cash',
+                'compare' => '='
+            )
+        )
     );
     $query = new WP_Query($args);
     if ($query->have_posts()) {
