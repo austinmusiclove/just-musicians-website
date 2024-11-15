45dc6afa9cc9477be48beb178e850cda1b7f2853
John Filippone <admin@austinmusiclove.com>
2024-11-14 18:32:00 -0600

    Changes env var name for google creds

--- wp-content/mu-plugins/social-login.php
+++ wp-content/mu-plugins/social-login.php
@@ -16,9 +16,9 @@ defined( 'ABSPATH' ) or die();
 // call sdk library
 require_once 'google-api/vendor/autoload.php';
 
-$GOOGLE_CLIENT_ID = GB_GOOGLE_WEB_OAUTH_CLIENT_ID;
-$GOOGLE_CLIENT_SECRET = GB_GOOGLE_WEB_OAUTH_CLIENT_SECRET;
-$GOOGLE_REDIRECT_URI = GB_GOOGLE_WEB_OAUTH_REDIRECT_URI;
+$GOOGLE_CLIENT_ID = JM_GOOGLE_WEB_OAUTH_CLIENT_ID;
+$GOOGLE_CLIENT_SECRET = JM_GOOGLE_WEB_OAUTH_CLIENT_SECRET;
+$GOOGLE_REDIRECT_URI = JM_GOOGLE_WEB_OAUTH_REDIRECT_URI;
 $GOOGLE_APPLICATION_NAME = "GigBuddy";
 $GOOGLE_SCOPES = "https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email";
 
