7bcd2421ff9e23284b43b47a7bfbe9b26577ec27
John Filippone <admin@austinmusiclove.com>
2024-12-10 00:23:37 -0600

    Adds uuid artist api

--- wp-content/mu-plugins/artists-api.php
+++ wp-content/mu-plugins/artists-api.php
@@ -15,6 +15,22 @@ function get_artist_post_id() {
     }
     return 0;
 }
+function get_artist_uuid() {
+    $artist_id = $_GET['artist_id'];
+
+    $args = array(
+        'post_type' => 'artist',
+        'posts_per_page' => 1,
+        'meta_key' => 'artist_id',
+        'meta_value' => $artist_id,
+    );
+    $query = new WP_Query($args);
+    if ($query->have_posts()) {
+        $query->the_post();
+        return get_field('artist_uuid');
+    }
+    return 0;
+}
 function get_artist() {
     $artist_uuid = $_GET['artist_uuid'];
 
@@ -60,6 +76,12 @@ add_action('rest_api_init', function () {
     register_rest_route( 'v1', 'artists/post_id', [
         'methods' => 'GET',
         'callback' => 'get_artist_post_id',
+    ]);
+});
+add_action('rest_api_init', function () {
+    register_rest_route( 'v1', 'artists/uuid', [
+        'methods' => 'GET',
+        'callback' => 'get_artist_uuid',
     ]);
 });
 add_action('rest_api_init', function () {
