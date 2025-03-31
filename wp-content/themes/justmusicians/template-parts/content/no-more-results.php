05cba8f532383afe89060e766b32b86a2c1ccfa7
John Filippone <admin@austinmusiclove.com>
2025-03-31 18:13:15 -0500

    Changes js to alpine in no more results template

--- wp-content/themes/justmusicians/template-parts/content/no-more-results.php
+++ wp-content/themes/justmusicians/template-parts/content/no-more-results.php
@@ -1,7 +1,7 @@
 <div class="font-sun-motter text-center px-4 pb-28 pt-12 sm:py-20 relative mb-4 xl:mb-0">
 
     <span class="text-20 block text-center mb-6 relative z-10">We've reached the end of the trail.</span>
-    <button onclick="event.preventDefault(); window.scrollTo({ top: 0, behavior: 'smooth' });" class="text-12 rounded-sm p-3 bg-yellow-light relative z-10">Scroll to Top</button>
+    <button type="button" x-on:click="window.scrollTo({ top: 0, behavior: 'smooth' });" class="text-12 rounded-sm p-3 bg-yellow-light relative z-10">Scroll to Top</button>
 
     <img class="w-40 absolute bottom-0 left-0 z-0" src="<?php echo get_template_directory_uri() . '/lib/images/other/cactus.svg'; ?>" />
     <img class="w-40 absolute bottom-0 right-0 z-0" src="<?php echo get_template_directory_uri() . '/lib/images/other/tumbleweed.svg'; ?>" />
