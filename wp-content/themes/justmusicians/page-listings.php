40588b5b232c75be07dd82f93b7063f40f4cdff2
Devon Bussell <devoncbussell@gmail.com>
2025-04-08 11:17:43 -0400

    added number of results

---
+++ wp-content/themes/justmusicians/page-listings.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The template for the listings landing page
+ *
+ * @package JustMusicians
+ */
+
+get_header();
+
+?>
+<header class="bg-yellow-light pt-12 md:pt-24 pb-8 md:pb-16 relative overflow-hidden">
+    <div class="container grid grid-cols-1 sm:grid-cols-7 gap-x-8 md:gap-x-24 gap-y-10 relative">
+        <h1 class="font-bold text-32 md:text-36 lg:text-40"><?php the_title(); ?></h1>
+    </div>
+</header>
+
+<div class="container lg:grid lg:grid-cols-10 gap-24 py-8 min-h-[500px]">
+    <div class="col lg:col-span-7 article-body mb-8 lg:mb-0">
+
+    <?php if (is_user_logged_in()) {
+        if (isset($_GET['lic'])) {
+            $success = add_listing_by_invitation_code($_GET['lic']);
+            // if success, redirect back to the same page but remove query params to avoid an infinite loop
+            if ($success and !is_wp_error($success)) { ?>
+                <div x-init="redirect('<?php echo strtok($_SERVER['REQUEST_URI'], '?'); ?>')"></div>
+            <?php } else { ?>
+                <p>Failed to add listing from invitation link with error: <span class="text-yellow"><?php echo $success->get_error_message(); ?></span></p>
+            <?php }
+        } ?>
+
+        <button type="button" href="" class="mb-8 border bg-yellow hover:bg-navy hover:text-white">Create Listing</button>
+
+
+        <?php
+        // Get listings
+        $current_user = wp_get_current_user();
+        $listing_ids = get_user_meta($current_user->ID, 'listings', true);
+
+
+        if ($listing_ids && is_array($listing_ids)) {
+            $args = array(
+                'post_type' => 'listing',
+                'posts_per_page' => -1,
+                'post_status' => 'publish',
+                'post__in' => $listing_ids,
+            );
+            $query = new WP_Query($args);
+            if ($query->have_posts()) { ?>
+                <?php while ($query->have_posts()) {
+
+
+                    // Display listings
+                    $query->the_post(); ?>
+                    <div><?php the_title(); ?></div>
+
+
+                <?php }
+            } else {
+                echo '<p>You don\'t have any listings associated with your account.</p>';
+            }
+            wp_reset_postdata(); // Reset the query
+        } else {
+            echo '<p>You don\'t have any listings associated with your account.</p>';
+        }
+    } else {
+        if (!empty($_GET['lic']) and !empty($_GET['mdl']) and $_GET['mdl'] == 'signup') { ?>
+            <span x-init="showLoginModal = false; showSignupModal = true; signupModalMessage = 'Sign up to activate this listing invitation link';"></span>
+        <?php } else if (isset($_GET['lic'])) { ?>
+            <span x-init="showLoginModal = true; showSignupModal = false; loginModalMessage = 'Sign in to activate this listing invitation link';"></span>
+        <?php } else { ?>
+            <span x-init="showLoginModal = true; showSignupModal = false; loginModalMessage = 'Sign in to see your listings';"></span>
+        <?php }
+    } ?>
+    </div>
+
+</div>
+
+
+<?php
+get_footer();
+
--- wp-content/themes/justmusicians/template-parts/search/sort.php
+++ wp-content/themes/justmusicians/template-parts/search/sort.php
@@ -1,28 +1,41 @@
-<div class="text-16 flex items-center gap-2 py-2">
-    <div class="flex items-center gap-1.5 group relative h-50">
-        <div id="tooltip-sort" class="tooltip text-white bg-black px-4 py-3 text-14 rounded hidden group-hover:block absolute z-50 w-64 -top-[65px] -right-28 md:right-auto">
-          Learn more about the default Just Musicians search algorithm <a class="text-yellow underline" href="#">here</a>.
+<div class="text-16 py-2 flex items-center gap-2">
+
+    <!-- Sort -->
+    <div class="flex items-center gap-2">
+        <div class="flex items-center gap-1.5 group relative h-50">
+            <div id="tooltip-sort" class="tooltip text-white bg-black px-4 py-3 text-14 rounded hidden group-hover:block absolute z-50 w-64 -top-[65px] -right-28 md:right-auto">
+            Learn more about the default Just Musicians search algorithm <a class="text-yellow underline" href="#">here</a>.
+            </div>
+            <img id="info-sort" class="opacity-40 h-4 cursor-pointer hover:opacity-100" src="<?php echo get_template_directory_uri() . '/lib/images/icons/circle-info.svg'; ?>"/>
         </div>
-        <img id="info-sort" class="opacity-40 h-4 cursor-pointer hover:opacity-100" src="<?php echo get_template_directory_uri() . '/lib/images/icons/circle-info.svg'; ?>"/>
-    </div>
-    <div class="flex items-center gap-1.5 group relative">
-        Sort:
-        <span class="font-bold flex items-center">
-            Default
-            <img class="ml-1.5" src="<?php echo get_template_directory_uri() . '/lib/images/icons/caret-down.svg'; ?>" />
-        </span>
-        <div class="absolute top-full w-40 px-4 py-4 bg-white hidden group-hover:flex flex-col shadow-md rounded-sm z-10 right-0">
-            <a class="px-2 py-1.5 flex items-center gap-2 hover:bg-yellow-light/50 rounded-sm" href="#">
+        <div class="flex items-center gap-1.5 group relative">
+            Sort:
+            <span class="font-bold flex items-center">
                 Default
-            </a>
-            <!-- Unsupported search options
-            <a class="px-2 py-1.5 flex items-center gap-2 hover:bg-yellow-light/50 rounded-sm" href="#">
-                Highest Rated
-            </a>
-            <a class="px-2 py-1.5 flex items-center gap-2 hover:bg-yellow-light/50 rounded-sm" href="#">
-                Most Reviewed
-            </a>
-            -->
+                <img class="ml-1.5" src="<?php echo get_template_directory_uri() . '/lib/images/icons/caret-down.svg'; ?>" />
+            </span>
+            <div class="absolute top-full w-40 px-4 py-4 bg-white hidden group-hover:flex flex-col shadow-md rounded-sm z-10 right-0">
+                <a class="px-2 py-1.5 flex items-center gap-2 hover:bg-yellow-light/50 rounded-sm" href="#">
+                    Default
+                </a>
+                <!-- Unsupported search options
+                <a class="px-2 py-1.5 flex items-center gap-2 hover:bg-yellow-light/50 rounded-sm" href="#">
+                    Highest Rated
+                </a>
+                <a class="px-2 py-1.5 flex items-center gap-2 hover:bg-yellow-light/50 rounded-sm" href="#">
+                    Most Reviewed
+                </a>
+                -->
+            </div>
         </div>
     </div>
+
+    <!-- Number of Results -->
+
+    <div class="flex items-center gap-2">
+        <div class="h-5 w-px bg-black/20"></div>
+        <span>42 results</span>
+    </div>
+
+
 </div>
