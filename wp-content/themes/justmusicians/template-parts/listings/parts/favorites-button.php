026a00a46523f630e5d7dbff5b05e9e1db165c69
Devon Bussell <devoncbussell@gmail.com>
2025-07-18 13:23:20 -0400

    updates to add to collections ui

--- wp-content/themes/justmusicians/template-parts/listings/parts/favorites-button.php
+++ wp-content/themes/justmusicians/template-parts/listings/parts/favorites-button.php
@@ -26,7 +26,10 @@
             x-on:click="showSignupModal = true; signupModalMessage = 'Sign up for an account to save listings'"
         <?php } ?>
     >
-        <img class="h-6 w-6" src="<?php echo get_template_directory_uri() . '/lib/images/icons/favorite.svg'; ?>" />
+    <svg class="w-4 h-auto fill-navy opacity-60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+        <path d="M0 48C0 21.5 21.5 0 48 0l0 48 0 393.4 130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4 336 48 48 48 48 0 336 0c26.5 0 48 21.5 48 48l0 440c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488L0 48z"/>
+    </svg>
     </button>
     <button type="button" class="opacity-60 hover:opacity-100 hover:scale-105"
         x-show="_showFilledFavoriteButton('<?php echo $args['post_id']; ?>')" x-cloak
@@ -36,7 +39,10 @@
         hx-trigger="remove-from-favorites"
         hx-indicator="#decoy-indicator"
     >
-        <img class="h-6 w-6" src="<?php echo get_template_directory_uri() . '/lib/images/icons/favorite-red.svg'; ?>" />
+         <svg class="w-4 h-auto fill-navy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+            <path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"/>
+        </svg>
     </button>
     <span id="favorites-result-<?php echo $args['post_id']; ?>"></span>
 
@@ -53,6 +59,10 @@
             x-intersect:leave="_resetCollectionsPopup()"
         >
 
+            <div class="flex items-center justify-between pr-3">
+                <span class="font-bold text-20">Add to collection</span>
+                <svg class="w-4 opacity-40 cursor-pointer hover:opacity-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>
+            </div>
             <!-- Search Bar -->
             <input type="text" placeholder="Search collections..." class="w-full px-3 py-1.5 border rounded focus:outline-none focus:ring focus:border-black text-sm"
                 x-model="collectionSearchQuery"
@@ -67,7 +77,7 @@
                     >
                         <a class="w-full" x-bind:href="collection.permalink"><span x-text="collection.name"></span></a>
                         <!-- Empty bookmark state -->
-                        <button type="button" class="mr-2"
+                        <button type="button" class="w-6 flex items-center"
                             x-show="_showEmptyCollectionButton(collection.post_id, '<?php echo $args['post_id']; ?>')" x-cloak
                             x-bind:hx-post="'<?php echo site_url(); ?>/wp-html/v1/collections/' + collection.post_id + '/listings/<?php echo $args['post_id']; ?>'"
                             hx-target="#favorites-result-<?php echo $args['post_id']; ?>"
@@ -75,22 +85,21 @@
                             hx-indicator="#decoy-indicator"
                             hx-vals='{"listing_id": "<?php echo $args['post_id']; ?>"}'
                         >
-                            <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M5 5v14l7-5 7 5V5a2 2 0 00-2-2H7a2 2 0 00-2 2z" />
+                            <svg class="w-3 opacity-40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                                <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/>
                             </svg>
+
                         </button>
                         <!-- Filled bookmark state -->
-                        <button type="button" class="mr-2"
+                        <button type="button" class="w-6 flex items-center"
                             x-show="_showFilledCollectionButton(collection.post_id, '<?php echo $args['post_id']; ?>')" x-cloak
                             x-bind:hx-delete="'<?php echo site_url(); ?>/wp-html/v1/collections/' + collection.post_id + '/listings/<?php echo $args['post_id']; ?>'"
                             hx-target="#favorites-result-<?php echo $args['post_id']; ?>"
                             hx-trigger="click"
                             hx-indicator="#decoy-indicator"
                         >
-                            <svg class="w-4 h-4 fill-yellow" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M5 3a2 2 0 00-2 2v16l9-6 9 6V5a2 2 0 00-2-2H5z" />
-                            </svg>
+                        <svg class="w-6 fill-yellow" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M382-208 122-468l90-90 170 170 366-366 90 90-456 456Z"/></svg>
                         </button>
                     </div>
                 </template>
@@ -98,11 +107,14 @@
 
             <!-- Create New Collection -->
             <div class="pt-3 border-t">
-                <button type="button" class="text-sm text-black font-medium hover:underline"
+                <button type="button" class="text-sm text-black font-medium w-full"
                     x-show="!showCreateCollectionInput"
                     x-on:click="showCreateCollectionInput = true; $nextTick(() => $refs.newCollectionInput<?php echo $args['post_id']; ?>.focus());"
                 >
-                    + Create new collection
+                    <span class="w-full flex items-center justify-between pr-4">
+                        <span>Create new collection</span>
+                        <span>+</span>
+                    </span>
                 </button>
 
                 <div x-show="showCreateCollectionInput" class="space-y-2">
