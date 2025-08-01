4efc378a477b7710c8aca1a6e794c1d499da297c
John Filippone <admin@austinmusiclove.com>
2025-08-01 14:01:14 -0500

    Adds message bubble icon

--- wp-content/themes/justmusicians/header.php
+++ wp-content/themes/justmusicians/header.php
@@ -198,7 +198,7 @@
                   Collections
                 </a>
                 <a class="px-2 py-1.5 flex items-center gap-2 rounded-sm" href="<?php echo site_url('/messages/'); ?>">
-                  <img class="w-4" src="<?php echo get_template_directory_uri() . '/lib/images/icons/list-solid.svg'; ?>" />
+                  <img class="w-4" src="<?php echo get_template_directory_uri() . '/lib/images/icons/speech-bubble.svg'; ?>" />
                   Messages
                 </a>
                 <a class="px-2 py-1.5 flex items-center gap-2 rounded-sm" href="<?php echo site_url('/inquiries/'); ?>">
--- wp-content/themes/justmusicians/template-parts/messages/inquiry-message.php
+++ wp-content/themes/justmusicians/template-parts/messages/inquiry-message.php
@@ -10,7 +10,7 @@
 
     <!-- Timestamp -->
     <template x-if="message.created_at">
-        <div class="text-center text-grey text-14" x-text="new Date(message.created_at.replace(' ', 'T') + 'Z').toLocaleString()"></div>
+        <div class="text-center text-grey text-14 my-2" x-text="new Date(message.created_at.replace(' ', 'T') + 'Z').toLocaleString()"></div>
     </template>
 
 
