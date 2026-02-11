7782032b8a1b37fe64cf431311bddb3ac1df8f90
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-02-11 13:54:02 +0700

    Disallows reviewing yourself as a buyer

--- wp-content/mu-plugins/reviews-api/create-review.php
+++ wp-content/mu-plugins/reviews-api/create-review.php
@@ -2,6 +2,11 @@
 
 function create_review($args) {
 
+    // If this is a buyer review and the author matches the reviewee, this is a user reviewing themselves. Disallow
+    if ($args['post_type'] == 'buyer_review' and $args['meta_input']['author'] == $args['meta_input']['reviewee']) {
+        return new WP_Error('creation_failed', 'Failed to create review. You are not allowed to review yourself.');
+    }
+
     // Create post
     $review_id = wp_insert_post($args);
     if (is_wp_error($review_id) || !$review_id) {
