1b941f254d654ec0031634d8de2bbdad36edd9a6
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-03-01 20:40:26 +0800

    Aligns musician earnings table headings to top

--- wp-content/themes/justmusicians/template-parts/musician-earnings-database/venue-table/venue-table-head.php
+++ wp-content/themes/justmusicians/template-parts/musician-earnings-database/venue-table/venue-table-head.php
@@ -1,8 +1,8 @@
 <thead>
     <tr class="border-b-2 border-black">
-        <th class="py-3 pr-4 font-bold">Venue</th>
-        <th class="py-3 pr-4 font-bold cursor-pointer select-none" x-on:click="sortBy('total_reports')">
-            <span class="flex items-center gap-1">
+        <th class="py-3 pr-4 font-bold align-top">Venue</th>
+        <th class="py-3 pr-4 font-bold cursor-pointer select-none align-top" x-on:click="sortBy('total_reports')">
+            <span class="flex items-start gap-1">
                 Total Reports
                 <span class="flex flex-col gap-1">
                     <span :class="{ 'opacity-100': sortKey === 'total_reports' && sortAsc, 'opacity-20': sortKey !== 'total_reports' || !sortAsc}">↑</span>
@@ -10,8 +10,8 @@
                 </span>
             </span>
         </th>
-        <th class="py-3 pr-4 font-bold cursor-pointer select-none" x-on:click="sortBy('median_earnings')">
-            <span class="flex items-center gap-1">
+        <th class="py-3 pr-4 font-bold cursor-pointer select-none align-top" x-on:click="sortBy('median_earnings')">
+            <span class="flex items-start gap-1">
                 <span>
                     Median Earnings
                     <?php echo get_template_part('template-parts/global/tooltip', '', [ 'tooltip' => 'Median of earnings per gig' ]); ?>
@@ -22,8 +22,8 @@
                 </span>
             </span>
         </th>
-        <th class="py-3 pr-4 font-bold cursor-pointer select-none" x-on:click="sortBy('average_earnings')">
-            <span class="flex items-center gap-1">
+        <th class="py-3 pr-4 font-bold cursor-pointer select-none align-top" x-on:click="sortBy('average_earnings')">
+            <span class="flex items-start gap-1">
                 <span>
                     Average Earnings
                     <?php echo get_template_part('template-parts/global/tooltip', '', [ 'tooltip' => 'The average amount earned per gig' ]); ?>
@@ -34,15 +34,15 @@
                 </span>
             </span>
         </th>
-        <th class="py-3 pr-4 font-bold">Average Ensemble Size</th>
-        <th class="py-3 pr-4 font-bold">Average Set Length (minutes)</th>
-        <th class="py-3 pr-4 font-bold">
+        <th class="py-3 pr-4 font-bold align-top">Average Ensemble Size</th>
+        <th class="py-3 pr-4 font-bold align-top">Average Set Length (minutes)</th>
+        <th class="py-3 pr-4 font-bold align-top">
             <span>
                 Payment Method
                 <?php echo get_template_part('template-parts/global/tooltip', '', [ 'tooltip' => 'Most commonly reported payment method' ]); ?>
             </span>
         </th>
-        <th class="py-3 font-bold">
+        <th class="py-3 font-bold align-top">
             <span>
                 Payment Speed
                 <?php echo get_template_part('template-parts/global/tooltip', '', [ 'tooltip' => 'Most commonly reported payment speed' ]); ?>
