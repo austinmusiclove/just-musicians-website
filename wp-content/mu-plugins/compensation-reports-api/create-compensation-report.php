3468471dfab27efb3434a9c41e0b27b030aec8c3
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-03-01 20:57:45 +0800

    Adds email to notify comp report submission

--- wp-content/mu-plugins/compensation-reports-api/create-compensation-report.php
+++ wp-content/mu-plugins/compensation-reports-api/create-compensation-report.php
@@ -15,6 +15,17 @@ function create_compensation_report($args) {
         return new WP_Error('creation_failed', 'Failed to create report.');
     }
 
+    // Send email to author
+    $email = $args['meta_input']['author_email'];
+    $subject = "Your Anonymous Live Musician Earnings Report Has Been Submitted";
+    $earnings_database_url = site_url('/musician-earnings-database/');
+    $message = "Thank you for contributing your earnings data!" . "\n\n";
+    $message .= "Your report for {$args['meta_input']['venue_name']} has been submitted and is currently pending review." . "\n\n";
+    $message .= "Your submission is completely anonymous. We will never share your name, show dates, or any other personal details." . "\n\n";
+    $message .= "Once our team verifies the authenticity of your report, it will be included in the musician earnings database." . "\n\n";
+    $message .= "You can view the database here: {$earnings_database_url}";
+    send_email_safely($email, $subject, $message);
+
     return [
         'post_id' => $post_id,
     ];
