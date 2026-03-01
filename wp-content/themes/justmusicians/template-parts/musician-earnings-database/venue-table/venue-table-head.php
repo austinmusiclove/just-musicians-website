0b17e1b06098001247fb4d57cadf5245693a42c6
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-03-01 18:24:37 +0800

    Adds sort to musician earnings database

--- wp-content/themes/justmusicians/template-parts/musician-earnings-database/venue-table/venue-table-head.php
+++ wp-content/themes/justmusicians/template-parts/musician-earnings-database/venue-table/venue-table-head.php
@@ -1,9 +1,18 @@
 <thead>
     <tr class="border-b-2 border-black">
         <th class="py-3 pr-4 font-bold">Venue</th>
-        <th class="py-3 pr-4 font-bold">Total Reports</th>
-        <th class="py-3 pr-4 font-bold">Median Earnings</th>
-        <th class="py-3 pr-4 font-bold">Average Earnings</th>
+        <th class="py-3 pr-4 font-bold cursor-pointer select-none" x-on:click="sortBy('total_reports')">
+            Total Reports
+            <span x-show="sortKey === 'total_reports'" x-text="sortAsc ? '↑' : '↓'" class="ml-1"></span>
+        </th>
+        <th class="py-3 pr-4 font-bold cursor-pointer select-none" x-on:click="sortBy('median_earnings')">
+            Median Earnings
+            <span x-show="sortKey === 'median_earnings'" x-text="sortAsc ? '↑' : '↓'" class="ml-1"></span>
+        </th>
+        <th class="py-3 pr-4 font-bold cursor-pointer select-none" x-on:click="sortBy('average_earnings')">
+            Average Earnings
+            <span x-show="sortKey === 'average_earnings'" x-text="sortAsc ? '↑' : '↓'" class="ml-1"></span>
+        </th>
         <th class="py-3 pr-4 font-bold">Avg Ensemble Size</th>
         <th class="py-3 pr-4 font-bold">Avg Set Length (min)</th>
         <th class="py-3 pr-4 font-bold">Payment Method</th>
--- wp-content/themes/justmusicians/template-parts/musician-earnings-database/venue-table/venue-table.php
+++ wp-content/themes/justmusicians/template-parts/musician-earnings-database/venue-table/venue-table.php
@@ -1,8 +1,25 @@
 <div class="overflow-x-auto"
     x-data="{
         venues: <?php echo clean_arr_for_doublequotes($args['venues']); ?>,
+        sortKey: 'total_reports',
+        sortAsc: false,
+        get sortedVenues() {
+            if (!this.sortKey) return this.venues;
+            return [...this.venues].sort((a, b) => {
+                const valA = a[this.sortKey];
+                const valB = b[this.sortKey];
+                return this.sortAsc ? valA - valB : valB - valA;
+            });
+        },
+        sortBy(key) {
+            if (this.sortKey === key) {
+                this.sortAsc = !this.sortAsc;
+            } else {
+                this.sortKey = key;
+                this.sortAsc = false;
+            }
+        }
     }"
-    x-init="console.log(venues);"
 >
     <table class="w-full border-collapse text-left">
 
@@ -11,11 +28,7 @@
 
         <!-- Table body -->
         <tbody>
-            <?php foreach ($args['venues'] as $venue) :
-                //echo get_template_part('template-parts/musician-earnings-database/venue-table/venue-table-row', '', [ 'venue' => $venue ]);
-            endforeach; ?>
-
-            <template x-for="venue in venues" :key="venue.ID">
+            <template x-for="venue in sortedVenues" :key="venue.ID">
                 <tr class="border-b border-gray-300 hover:bg-gray-50">
 
                     <td class="py-3 pr-4">
