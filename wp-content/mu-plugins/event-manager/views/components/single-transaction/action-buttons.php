2f4ef2f5bbf1e72d31ee2136d0624199db93e9cc
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-05-04 21:24:10 +0900

    Adds next button

--- wp-content/mu-plugins/event-manager/views/components/single-transaction/action-buttons.php
+++ wp-content/mu-plugins/event-manager/views/components/single-transaction/action-buttons.php
@@ -3,13 +3,20 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 ?>
-<div style="margin-top: 30px; padding-top: 20px; border-top: 1px solid #c3c4c7;">
-    <button type="submit" name="em_approve" value="1" class="button button-primary" id="em-approve-transaction">
-        Approve Transaction
-    </button>
-    <button type="submit" name="em_reject" value="1" class="button button-secondary" id="em-reject-transaction" style="margin-left: 8px;">
-        Reject Transaction
-    </button>
+<div style="margin-top: 30px; padding-top: 20px; border-top: 1px solid #c3c4c7; display: flex; align-items: center;">
+    <div>
+        <button type="submit" name="em_approve" value="1" class="button button-primary" id="em-approve-transaction">
+            Approve Transaction
+        </button>
+        <button type="submit" name="em_reject" value="1" class="button button-secondary" id="em-reject-transaction" style="margin-left: 8px;">
+            Reject Transaction
+        </button>
+    </div>
+    <div style="margin-left: auto;">
+        <button type="button" class="button button-secondary" id="em-next-transaction">
+            Next &rarr;
+        </button>
+    </div>
 </div>
 
 <?php if ( ! empty( $approve_result ) ) : ?>
