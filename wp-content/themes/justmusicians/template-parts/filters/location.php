59dea2ca3bace4ac10499262f5962507f5674550
John Filippone <johnfilippone@Johns-MacBook-Air.local>
2026-06-02 17:53:09 +0200

    Fixes location input style

--- wp-content/themes/justmusicians/template-parts/filters/location.php
+++ wp-content/themes/justmusicians/template-parts/filters/location.php
@@ -1,8 +1,10 @@
 <div class="border-b border-black/20 mb-6 pb-6 last:mb-0 last:pb-0 last:border-b-0">
     <h3 class="font-bold text-18 mb-3">Location</h3>
     <form class="flex flex-col gap-y-2">
-        <label for="zipcode" class="hidden">Enter zipcode</label>
-        <input id="zipcode" type="number" name="zipcode" placeholder="Enter zip code" />
+        <div class="relative flex items-center">
+            <img class="h-4 absolute left-2" src="<?php echo get_template_directory_uri() . '/lib/images/icons/location.svg'; ?>" />
+            <input class="w-full py-2 pr-3 pl-5 no-formatting border border-black/20 focus:border-black outline-none" type="text" name="location" placeholder="Enter city or postal code" />
+        </div>
         <label for="radius" class="hidden">Radius</label>
         <select id="radius" name="radius">
             <option value="" disabled selected hidden>Radius</option>
