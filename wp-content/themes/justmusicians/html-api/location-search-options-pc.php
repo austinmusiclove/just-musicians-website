169f2349d5f903834d9e9e2a645d5ad2457b266c
John Filippone <admin@austinmusiclove.com>
2026-06-22 08:30:01 +0200

    Adds no results message for location active search

--- wp-content/themes/justmusicians/html-api/location-search-options-pc.php
+++ wp-content/themes/justmusicians/html-api/location-search-options-pc.php
@@ -11,8 +11,14 @@ if (empty($search_term)) {
     ]);
 } else {
     $result = hm_location_search($search_term, false);
-    get_template_part('template-parts/search/active-search/location-search-state-2', '', [
-        'locations' => $result,
-        'update_func' => $update_func,
-    ]);
+    if (count($result) == 0) {
+        get_template_part('template-parts/search/active-search/location-search-state-1', '', [
+            'message' => 'No Results. Enter a valid US or Canada postal code (ex. 78701, A1A)'
+        ]);
+    } else {
+        get_template_part('template-parts/search/active-search/location-search-state-2', '', [
+            'locations' => $result,
+            'update_func' => $update_func,
+        ]);
+    }
 }
