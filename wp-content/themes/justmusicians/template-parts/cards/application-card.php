0fa3f163ed4f89ff22760c174df00d3fd14de07a
John Filippone <admin@austinmusiclove.com>
2026-07-02 09:25:44 +0200

    Alters one color on application card

--- wp-content/themes/justmusicians/template-parts/cards/application-card.php
+++ wp-content/themes/justmusicians/template-parts/cards/application-card.php
@@ -17,7 +17,7 @@
         </div>
 
         <!-- Application Link -->
-        <div class="text-yellow">
+        <div class="text-yellow/50">
             <?php echo get_template_part('template-parts/global/copy-to-clipboard', '', [
                 'text' => get_musician_application_url($args['post_id']),
                 'external_link' => esc_url(get_musician_application_url($args['post_id'])),
