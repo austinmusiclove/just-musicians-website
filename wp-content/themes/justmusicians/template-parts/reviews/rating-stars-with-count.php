07fd66e4a86cfd7ce57bc7c9787bf0dc7b8f1b0e
John Filippone <admin@austinmusiclove.com>
2026-07-29 03:55:29 +0300

    Hides rating stars if there are 0 reviews

--- wp-content/themes/justmusicians/template-parts/reviews/rating-stars-with-count.php
+++ wp-content/themes/justmusicians/template-parts/reviews/rating-stars-with-count.php
@@ -1,12 +1,16 @@
-<div class="flex gap-2">
+<?php if ($args['review_count'] > 0) { ?>
 
-    <div class="flex gap-x-1 text-yellow w-24">
-        <?php echo get_template_part('template-parts/reviews/rating-stars-display', '', [ 'rating' => $args['rating'], ]); ?>
+    <div class="flex gap-2">
+
+        <div class="flex gap-x-1 text-yellow w-24">
+            <?php echo get_template_part('template-parts/reviews/rating-stars-display', '', [ 'rating' => $args['rating'], ]); ?>
+        </div>
+
+        <span class="font-normal text-12">
+            (<?php echo $args['review_count']; ?>
+            <?php echo ( (int) $args['review_count'] === 1 ) ? 'review' : 'reviews'; ?>)
+        </span>
+
     </div>
 
-    <span class="font-normal text-12">
-        (<?php echo $args['review_count']; ?>
-        <?php echo ( (int) $args['review_count'] === 1 ) ? 'review' : 'reviews'; ?>)
-    </span>
-
-</div>
+<?php } ?>
