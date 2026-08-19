d62e7dbe31f0e4ead657da756899d805ff9ca814
John Filippone <admin@austinmusiclove.com>
2026-08-19 16:05:26 +0300

    Adds register existing user test

--- wp-content/themes/justmusicians/tests/tests/e2e/users/register-user.spec.js
+++ wp-content/themes/justmusicians/tests/tests/e2e/users/register-user.spec.js
@@ -1,5 +1,6 @@
 import { expect } from '@playwright/test';
 import { test } from '../../../fixtures/fixtures.js';
+import { createUser } from '../../../data/factories/user_factory.js';
 
 test.describe('E2E - Register User', () => {
 
@@ -8,6 +9,7 @@ test.describe('E2E - Register User', () => {
         const user = await themePage.registerUserSignupModal();
         wpCli.trackUser(user);
         await themePage.expectLoggedInPage();
+        await expect(themePage.page).toHaveURL('/');
 
         const adminEmail = await mailpit.findEmailBySubject('[Hire Musicians] New User Registration');
         expect(adminEmail).toBeTruthy();
@@ -23,17 +25,21 @@ test.describe('E2E - Register User', () => {
         const user = await themePage.registerUserSignupModal();
         wpCli.trackUser(user);
         await themePage.expectLoggedInPage();
-
-        const adminEmail = await mailpit.findEmailBySubject('[Hire Musicians] New User Registration');
-        expect(adminEmail).toBeTruthy();
-
-        const verifyEmail = await mailpit.findEmailBySubject(
-            `(${mailpit.siteUrl} ${user.email}) Verify your email to activate your Hire Musicians account`
-        );
-        expect(verifyEmail).toBeTruthy();
+        await expect(themePage.page).toHaveURL(/\/blog\/\?arg=abc/);
     });
 
-    test.skip('new user is redirected to the same url that they were at when they registered successfully with query params', async ( {} ) => {});
+    test('sign up user that already exists', async ({ themePage, wpCli }) => {
+        const existingUser = createUser();
+        wpCli.createUser(existingUser);
+
+        await themePage.navigate('/');
+        await themePage.openSignupModal();
+        await themePage.fillSignupForm(existingUser);
+        await themePage.signupSubmitBtn.click();
+
+        await expect(themePage.page.locator('#sign-up-result')).toContainText('Email already registered');
+    });
+
     test.skip('account activation email is sent to new user after registration', async ( {} ) => {}); // Maybe best for a PHP test
     test.skip('sign up with google', async ( {} ) => {}); // Mock the google part
 
