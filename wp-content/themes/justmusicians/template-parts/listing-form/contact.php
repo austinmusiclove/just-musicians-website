67a7fc942dcf020e9b8f7c58ee597e140011b509
John Filippone <admin@austinmusiclove.com>
2026-08-25 16:21:51 +0300

    Adds asterisk for email in listing form

--- wp-content/themes/justmusicians/template-parts/listing-form/contact.php
+++ wp-content/themes/justmusicians/template-parts/listing-form/contact.php
@@ -11,7 +11,7 @@
         <div class="grid sm:grid-cols-3 gap-2">
             <!-- Email -->
             <div class="grow relative">
-                <label for="listing_email" class="mb-1 inline-block">Email (not public)</label>
+                <label for="listing_email" class="mb-1 inline-block">Email <span class="text-red">*</span> (not public)</label>
                 <img class="h-5 absolute bottom-2.5 left-3" src="<?php echo get_template_directory_uri() . '/lib/images/icons/email.svg'; ?>" />
                 <input class="has-icon" type="text" id="listing_email" name="listing_email" autocomplete="email" placeholder="example@example.com" title="example@example.com" required x-model="pEmail" >
             </div>
