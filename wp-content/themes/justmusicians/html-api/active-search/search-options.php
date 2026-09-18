e991cad247b8edbc4ea787030823a088416233ef
John Filippone <admin@austinmusiclove.com>
2026-09-18 23:39:14 +0100

    Makes sure active search does not get listings without location args

--- wp-content/themes/justmusicians/html-api/active-search/search-options.php
+++ wp-content/themes/justmusicians/html-api/active-search/search-options.php
@@ -1,30 +1,30 @@
 <?php
 $search_term = stripslashes($_GET['s'] ?? '');
-$lat = !empty($_GET['lat']) ? stripslashes($_GET['lat']) : null;
-$lng = !empty($_GET['lng']) ? stripslashes($_GET['lng']) : null;
+$lat = is_numeric($_GET['lat'] ?? '') ? (float) stripslashes($_GET['lat']) : null;
+$lng = is_numeric($_GET['lng'] ?? '') ? (float) stripslashes($_GET['lng']) : null;
 if (empty($search_term)) {
     get_template_part('template-parts/search/active-search/search-state-1');
 } else {
-    $get_listings_args = [
-        'name_search' => $search_term,
-    ];
+    $listings = [];
     if ($lat !== null && $lng !== null) {
-        $get_listings_args['lat'] = $lat;
-        $get_listings_args['lng'] = $lng;
+        $result = get_listings([
+            'name_search' => $search_term,
+            'lat'         => $lat,
+            'lng'         => $lng,
+        ]);
+        $listings = !empty($result['listings']) ? $result['listings'] : [];
     }
-    $result = get_listings($get_listings_args);
-    $listings = $result['listings'];
     $categories       = get_terms_decoded('mcategory', 'names', $search_term, true);
     $genres           = get_terms_decoded('genre', 'names', $search_term, true);
     $subgenres        = get_terms_decoded('subgenre', 'names', $search_term, true);
     $instrumentations = get_terms_decoded('instrumentation', 'names', $search_term, true);
     $settings         = get_terms_decoded('setting', 'names', $search_term, true);
-    get_template_part('template-parts/search/active-search/search-state-2', '', array(
+    get_template_part('template-parts/search/active-search/search-state-2', '', [
         'listings' => $listings,
         'categories' => $categories,
         'genres' => $genres,
         'subgenres' => $subgenres,
         'instrumentations' => $instrumentations,
         'settings' => $settings,
-    ));
+    ]);
 }
